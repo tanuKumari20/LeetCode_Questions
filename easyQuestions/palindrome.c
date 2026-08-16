10b043a6d173ad75630e49d65c7f0aac957b7086
tanuKumari20 <tanukumari33333@gmail.com>
2026-08-17 00:02:11 +0530

    Add palindrome checking functionality
    Implement palindrome check for an integer input.

--- easyQuestions/palindrome.c
+++ easyQuestions/palindrome.c
@@ -1,1 +1,41 @@
+//Example 1:
+
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isPalindrome(int x);
+
+int main(){
+int x;
+    printf("Enter the number: ");
+    scanf("%d",&x);
+    if(isPalindrome(x)){
+        printf("true\n");
+    }
+    else{
+        printf("false\n ");
+    }
+}
+
+bool isPalindrome(int x) {
+    int rem;
+    long rev = 0, n;
+    n = x;
+
+    while (x > 0) {
+        rem = x % 10;
+        rev = rev * 10 + rem;
+        x = x / 10;
+    }
+    if (n == rev) {
+        return true;
+    } else {
+        return false;
+    }
+    return 0;
+}
 
